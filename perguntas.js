42f14cb03a8f220471b65de89c672d763806aa70
Ana Flavia Pedrão <a.pedrao@escola.pr.gov.br>
2025-08-13 20:03:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris.'
+)
+
+criaCartao(
+    'Matemática',
+    'O que são números primos?',
+    'São números naturais maiores que 1 que possuem exatamente dois divisores distintos: o número 1 e ele mesmo.'
+)
+
