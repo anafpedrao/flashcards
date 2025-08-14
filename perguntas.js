9b2d02222ae466feabe787941457312a8116cef5
Ana Flavia Pedrão <a.pedrao@escola.pr.gov.br>
2025-08-13 21:38:58 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,18 +1,55 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação.'
+    'História',
+    'Quem foi o primeiro presidente do Brasil?',
+    'O primeiro presidente do Brasil foi Marechal Deodoro da Fonseca.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris.'
+    'Ciências',
+    'Qual é o planeta mais próximo do Sol?',
+    'O planeta mais próximo do Sol é Mercúrio.'
 )
 
 criaCartao(
-    'Matemática',
-    'O que são números primos?',
-    'São números naturais maiores que 1 que possuem exatamente dois divisores distintos: o número 1 e ele mesmo.'
+    'Esportes',
+    'Qual país sediou a Copa do Mundo de 2014?',
+    'O Brasil sediou a Copa do Mundo de 2014.'
 )
+
+criaCartao(
+    'Arte',
+    'Quem pintou a Mona Lisa?',
+    'A Mona Lisa foi pintada por Leonardo da Vinci.'
+)
+
+criaCartao(
+    'Música',
+    'Qual é o nome do compositor da música “O Guarani”?',
+    'O compositor é Carlos Gomes.'
+)
+
+criaCartao(
+    'Português',
+    'O que é um adjetivo?',
+    'É a palavra que caracteriza ou descreve um substantivo.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é o maior animal do mundo?',
+    'A baleia-azul é o maior animal do mundo.'
+)
+
+criaCartao(
+    'Física',
+    'Qual é a unidade de medida da força no Sistema Internacional?',
+    'A unidade de medida da força é o Newton (N).'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o símbolo químico da água?',
+    'O símbolo químico da água é H₂O.'
+)
+
 
